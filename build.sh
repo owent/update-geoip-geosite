29eebf29c6d113273167bd1e996df0fabe46c39d
owentou <owentou@tencent.com>
2019-12-31 11:49:12 +0800

    add records

--- build.sh
+++ build.sh
@@ -24,6 +24,12 @@ go get -insecure -u github.com/v2ray/geoip ;
 $GOPATH/bin/geoip --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv ;
 
 # start to build geosite.dat
+
+GEOIP_DAT_URL="https://github.com/v2ray/geoip/releases/latest/download/geoip.dat";
+GFWLIST_ORIGIN_URL="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt";
+GFWLIST_GEN_SCRIPT_URL="https://raw.githubusercontent.com/cokebar/gfwlist2dnsmasq/master/gfwlist2dnsmasq.sh";
+# MYIP=$(curl https://myip.biturl.top/ 2>/dev/null);
+
 go get -insecure -v -t -d github.com/v2ray/domain-list-community/... ;
 go run ./src/github.com/v2ray/domain-list-community/main.go ;
 
